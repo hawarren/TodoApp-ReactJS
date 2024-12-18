5aeab5aff30e778dd155ab41a09608a40ce5e9f1
Hanif Warren <Hanif.warren@gmail.com>
2024-12-17 23:15:31 -0500

    Initial React app with azure function

---
+++ api/src/functions/todolist.js
@@ -0,0 +1,12 @@
+const { app } = require("@azure/functions")
+
+app.http("todolist", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`)
+
+    const name = request.query.get("name") || (await request.text()) || "world"
+    return { body: JSON.stringify({ message: `Hello, ${name}!` }) }
+  }
+})
--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from "react"
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    ;(async function () {
+      try {
+        const response = await fetch(`/api/todolist`)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const result = await response.json()
+        setData(result.message)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      }
+    })()
+  }, [])
+
+  return <div>{data}</div>
 }
 
-export default App;
+export default App
